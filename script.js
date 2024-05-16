30bf6956f4f6e6a2cff929757007922e06188776
hamuzah <37175583+hamuzah@users.noreply.github.com>
2024-05-16 08:22:14 +0300

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+
+	const minHeap = new MinHeap();
+    arr.forEach(length => minHeap.push(length));
+
+    let totalCost = 0;
+
+    while (minHeap.size() > 1) {
+        const first = minHeap.pop();
+        const second = minHeap.pop();
+        const cost = first + second;
+        totalCost += cost;
+        minHeap.push(cost);
+    }
+
+    return totalCost;
   
 }
 
