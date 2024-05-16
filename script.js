0a70c8574145ac70f265037f65dc737556142603
hamuzah <37175583+hamuzah@users.noreply.github.com>
2024-05-16 08:46:47 +0300

    created a submission

--- script.js
+++ script.js
@@ -3,21 +3,27 @@ function mincost(arr)
 //write your code here
 // return the min cost
 
-	const minHeap = new MinHeap();
-    arr.forEach(length => minHeap.push(length));
-
-    let totalCost = 0;
-
-    while (minHeap.size() > 1) {
-        const first = minHeap.pop();
-        const second = minHeap.pop();
-        const cost = first + second;
-        totalCost += cost;
-        minHeap.push(cost);
+	 priority_queue<int, vector<int>, greater<int> > pq(
+        arr, arr + n);
+ 
+    // Initialize result
+    int res = 0;
+ 
+    // While size of priority queue is more than 1
+    while (pq.size() > 1) {
+        // Extract shortest two ropes from pq
+        int first = pq.top();
+        pq.pop();
+        int second = pq.top();
+        pq.pop();
+ 
+        // Connect the ropes: update result and
+        // insert the new rope to pq
+        res += first + second;
+        pq.push(first + second);
     }
-
-    return totalCost;
-  
+ 
+    return res;
 }
 
 module.exports=mincost;
